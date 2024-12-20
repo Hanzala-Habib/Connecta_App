aa34982457cc5022534f225933e0b005ff42aa0e
Hanzala Habib <imhanzala444@gmail.com>
2024-12-20 20:30:58 +0500

    main screen

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,10 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text("data"),
+      ),
+    );
   }
 }
