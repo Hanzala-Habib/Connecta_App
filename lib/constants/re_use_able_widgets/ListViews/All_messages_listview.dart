f2be0dd7b60a90edc8fb4b44f27da7d60cd2d115
Hanzala Habib <imhanzala444@gmail.com>
2024-12-20 22:05:05 +0500

    basics setup file structure

---
+++ lib/constants/re_use_able_widgets/ListViews/All_messages_listview.dart
@@ -0,0 +1,34 @@
+
+import 'package:connecta_app/constants/re_use_able_widgets/Cicular_avatar/Avatar_circle.dart';
+import 'package:flutter/material.dart';
+
+class AllMessagesListview extends StatefulWidget {
+
+  const AllMessagesListview({super.key});
+  @override
+  State<AllMessagesListview> createState() => _AllMessagesListviewState();
+}
+
+class _AllMessagesListviewState extends State<AllMessagesListview> {
+  var arr=["Hanzala","Shahbaz","Wajeeha","Sejeela"];
+  var sub_title=["Habib","Rana","Umer","Fatima"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.separated(itemBuilder: (context, index) {
+        return ListTile(
+          leading: AvatarCircle(name_letter: arr[index].toString()),
+          trailing:const Text("29/03/2024"),
+          title:Text(arr[index],),
+          subtitle:Text(sub_title[index])
+        );
+
+      }, separatorBuilder: (context, index) {
+        return const Divider(
+          height: 20,
+          thickness: 0.8,
+        );
+      }, itemCount:arr.length),
+    );
+  }
+}
